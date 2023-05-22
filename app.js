017228668933eccff9bc05cd67fc315c8ddf3ae0
rabiulislam007 <rabiulislam9@gmail.com>
2023-05-22 21:17:01 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+//routing
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.end('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
